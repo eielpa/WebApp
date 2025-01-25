25c25a0313a7c48383e27458121020b604497122
uggenio <eielpa@outloook.com>
2025-01-25 13:11:26 +0100

    collegato postgres, model, dao per user

--- MovieMarket/movieMarket/src/main/java/com/demacs/moviemarket/persistence/dao/UserDao.java
+++ MovieMarket/movieMarket/src/main/java/com/demacs/moviemarket/persistence/dao/UserDao.java
@@ -1,5 +1,9 @@
 package com.demacs.moviemarket.persistence.dao;
 
+import com.demacs.moviemarket.persistence.model.User;
+
+import java.util.List;
+
 public interface UserDao {
     User findById(int id);
     List<User> findAll();
