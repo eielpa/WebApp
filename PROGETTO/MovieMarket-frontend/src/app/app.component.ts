442d4eb3e38aceafc238acf807b34adabfaf3a9a
Pulicano-jago-francesco <plcjfr03p30d086s@studenti.unical.it>
2025-01-30 11:54:43 +0100

    costruzione front-end w angular

--- PROGETTO/MovieMarket-frontend/src/app/app.component.ts
+++ PROGETTO/MovieMarket-frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HomePageComponent } from './home-page/home-page.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true, // Componente principale standalone
+  imports: [HomePageComponent], // Importa il componente della homepage
+  template: `<app-home-page></app-home-page>`,
+  styles: []
 })
 export class AppComponent {
   title = 'MovieMarket-frontend';
