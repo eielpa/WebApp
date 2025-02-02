244a687284a07d9fa19282b89d61e9263470b1c1
Pulicano-jago-francesco <plcjfr03p30d086s@studenti.unical.it>
2025-02-02 15:58:48 +0100

    reg

--- eugenioProva/inizioFrontend/moviemarket/src/app/registration/registration.component.ts
+++ eugenioProva/inizioFrontend/moviemarket/src/app/registration/registration.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Aggiungi FormsModule
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [FormsModule], // Importa FormsModule
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.css'
+  styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  // Oggetto che contiene i dati dell'utente
+  user = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    birthDate: '',
+    email: '',
+    password: ''
+  };
 
+  onSubmit() {
+    // Puoi implementare la logica per inviare i dati a un servizio o un'API
+    console.log('Dati registrazione:', this.user);
+  }
 }
