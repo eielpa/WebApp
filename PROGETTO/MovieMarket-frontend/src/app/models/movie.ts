d8b9a5dddc311c0050a831a5b7f6b486db9e4410
Pulicano-jago-francesco <plcjfr03p30d086s@studenti.unical.it>
2025-02-06 22:12:37 +0100

    update

--- PROGETTO/MovieMarket-frontend/src/app/models/movie.ts
+++
@@ -1,7 +0,0 @@
-export interface Movie {
-  id: number;
-  title: string;
-  description: string;
-  posterUrl: string;
-  downloadUrl: string;
-}
--- app/watchlist-page/watchlist-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-watchlist-page',
-    templateUrl: './watchlist-page.component.html',
-    standalone: true,
-    styleUrls: ['./watchlist-page.component.css']
-})
-export class WatchlistPageComponent {}
